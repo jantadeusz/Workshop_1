9d08943b3fabe9e4307114e02f039a4369e87aee
jantadeusz <tadeusz_jankowski@mail.com>
2018-07-12 13:03:48 +0200

    workshop1_ex1 100%

--- src/pl/coderslab/guessNumberV1/GuessNumberV1.java
+++ src/pl/coderslab/guessNumberV1/GuessNumberV1.java
@@ -1,5 +1,6 @@
 package pl.coderslab.guessNumberV1;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -10,23 +11,29 @@ public class GuessNumberV1 {
     }
 
     static String gameV1() {
-        String result = null;
+        String result;
         Random generator = new Random();
         Scanner scan = new Scanner(System.in);
         int correctNumber = generator.nextInt(100) + 1;
         System.out.println("Zgadnij liczbę z zakresu 1-100: ");
         while (true) {
-            int getValue = scan.nextInt();
-            if (getValue < correctNumber) {
-                result = "Podana liczba jest mniejsza niż szukana. Spróbuj ponownie";
-                System.out.println(result);
-            } else if (getValue > correctNumber) {
-                result = "Podana liczba jest większa niż szukana. Spróbuj ponownie";
-                System.out.println(result);
-            } else {
-                result = "Zgadłeś!";
-                System.out.println(result);
-                break;
+            try {
+                int getValue = scan.nextInt();
+                if (getValue < correctNumber) {
+                    result = "Za mało!";
+                    System.out.println(result);
+                } else if (getValue > correctNumber) {
+                    result = "Za dużo!";
+                    System.out.println(result);
+                } else {
+                    result = "Zgadłeś!";
+                    System.out.println(result);
+                    break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("To nie jest liczba");
+                System.out.println("Zgadnij liczbę z zakresu 1-100: ");
+                scan.nextLine();
             }
         }
         return result;
